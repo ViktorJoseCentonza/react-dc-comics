590a1f20a259c8910544ca0eca5c68a7ff973580
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-03-20 17:03:36 +0100

    add general nav-banner css

--- src/components/NavBanner.jsx
+++ src/components/NavBanner.jsx
@@ -1,31 +1,39 @@
 export default function navBanner() {
     return (
-        <section className="nav-banner"> {/*different component? */}
-            <div>
+        <section className="nav-banner d-flex"> {/*different component? */}
+            <div className="d-flex">
                 <div>
                     <img src="../public/img/buy-comics-digital-comics.png" alt="digital-comics-logo" />
-                    <h4>DIGITAL COMICS</h4>
                 </div>
+                <h4>DIGITAL COMICS</h4>
+            </div>
 
+            <div className="d-flex">
                 <div>
                     <img src="../public/img/buy-comics-merchandise.png" alt="dc-merchandise-logo" />
-                    <h4>DC MERCHANDISE</h4>
                 </div>
+                <h4>DC MERCHANDISE</h4>
+            </div>
 
+            <div className="d-flex">
                 <div>
                     <img src="../public/img/buy-comics-subscriptions.png" alt="subscriptions-logo" />
-                    <h4>SUBSCRIPTION</h4>
                 </div>
+                <h4>SUBSCRIPTION</h4>
+            </div>
 
+            <div className="d-flex">
                 <div>
                     <img src="../public/img/buy-comics-shop-locator.png" alt="shop-locator-logo" />
-                    <h4>COMIC SHOP LOCATOR</h4>
                 </div>
+                <h4>COMIC SHOP LOCATOR</h4>
+            </div>
 
+            <div className="d-flex">
                 <div>
                     <img src="../public/img/buy-dc-power-visa.svg" alt="dc-power-visa-logo" />
-                    <h4>DC POWER VISA</h4>
                 </div>
+                <h4>DC POWER VISA</h4>
             </div>
         </section>
     );
